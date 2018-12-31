b928fd85227558f8fa3a0164d3e4b3c922ea5812
Neto Falcone <netofalcone@gmail.com>
2018-12-31 04:19:17 -0200

    second commit

--- src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -1,5 +1,10 @@
 package pos_java_jdbc.pos_java_jdbc;
 
+import java.util.List;
+import java.util.*;
+
+import javax.swing.plaf.ListUI;
+
 import org.junit.Test;
 
 import conexaojdbc.SingleConnection;
@@ -15,11 +20,41 @@ public class TesteBancoJdbc{
 		Userposjava userposjava = new Userposjava();
 		
 		
-		userposjava.setId(6L);
-		userposjava.setNome("Falcone2");
-		userposjava.setEmail("falcone2@gmail.com");
+		userposjava.setId(10L);
+		userposjava.setNome("F3");
+		userposjava.setEmail("f@gmail.com");
 		
 		userPosDAO.salvar(userposjava);
 
 }
+	@Test
+	public void initListar() {		
+		UserPosDAO dao = new UserPosDAO();
+		try {
+			List<Userposjava> list = dao.listar();
+			
+			 for (Userposjava userposjava : list) {
+				System.out.println(userposjava); 
+				System.out.println("-------------");
+			 				
+			}
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			}				
+		}
+
+	@Test
+	public void initBuscar() {
+			
+		UserPosDAO dao = new UserPosDAO();
+		
+		try {
+			Userposjava userposjava = dao.buscar(2L);
+			
+			System.out.println(userposjava);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+}
